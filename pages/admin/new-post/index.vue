cb6076ec4bc6966a8130b564e306ecc16685e775
web2ls <web2ls@yandex.ru>
2019-04-30 19:05:39 +0300

    added new post form dummy

---
+++ pages/admin/new-post/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="admin-new-post-page">
+    <section class="new-post-form">
+      <form>
+        
+      </form>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
