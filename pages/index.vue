ccf57c49151253d72cb4c3262fef44babccdc479
web2ls <web2ls@yandex.ru>
2019-04-30 15:45:56 +0300

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,116 @@
+<template>
+    <div class="home-page">
+        <section class="intro">
+            <h1>Get the latest tech news!</h1>
+        </section>
+
+        <section class="featured-posts">
+            <nuxt-link to="/posts/1" class="post-preview">
+                <article>
+                    <div class="post-thumbnail" style="background: url(https://webfrontier.ru/images/slide1.jpg) no-repeat center; backgroundSize: 'cover'"></div>
+                    <div class="post-content">
+                        <div>Post title</div>
+                        <p>Preview Text</p>
+                    </div>
+                </article>
+            </nuxt-link>
+
+            <nuxt-link to="/posts/2" class="post-preview">
+                <article>
+                    <div class="post-thumbnail" style="background: url(https://webfrontier.ru/images/slide2.jpg) no-repeat center; backgroundSize: 'cover'"></div>
+                    <div class="post-content">
+                        <div>Post title</div>
+                        <p>Preview Text</p>
+                    </div>
+                </article>
+            </nuxt-link>
+        </section>
+    </div>
+</template>
+
+<script>
+import Logo from "~/components/Logo.vue";
+
+export default {
+  components: {
+    Logo
+  }
+};
+</script>
+
+<style>
+.intro {
+  height: 300px;
+  position: relative;
+  padding: 30px;
+  box-sizing: border-box;
+  background-position: center;
+  background-size: cover;
+}
+
+.intro h1 {
+  position: absolute;
+  top: 10%;
+  left: 5%;
+  width: 90%;
+  font-size: 1.5rem;
+  color: black;
+  background-color: rgb(211, 211, 211);
+  padding: 10px;
+  border-radius: 10px;
+  box-shadow: 3px 3px 3px black;
+  box-sizing: border-box;
+  border: 1px solid black;
+}
+
+@media (min-width: 768px) {
+  .intro h1 {
+    font-size: 2rem;
+  }
+}
+
+.featured-posts {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+
+.post-preview {
+  border: 1px solid #ccc;
+  box-shadow: 0 2px 2px #ccc;
+  background-color: white;
+  width: 90%;
+}
+
+a {
+  text-decoration: none;
+  color: black;
+}
+
+@media (min-width: 850px) {
+  .post-preview {
+    width: 400px;
+    margin: 10px;
+  }
+}
+
+.post-thumbnail {
+  width: 100%;
+  height: 200px;
+  background-position: center;
+  background-size: cover;
+}
+
+.post-content {
+  padding: 10px;
+  text-align: center;
+}
+
+a:hover .post-content,
+a:active .post-content {
+  background-color: #ccc;
+}
+</style>
---
+++ pages/posts/_id/index.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="single-post-page">
+    <section class="post">
+      <h1>Title of the post</h1>
+      <div class="post-details">
+        <div>Last updated DATE</div>
+        <div>Written by NAME</div>
+      </div>
+
+      <div class="content">
+        Post content
+      </div>
+
+      <div class="post-feedback">
+        <p>field for feedback</p>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.single-post-page {
+  padding: 30px;
+  text-align: center;
+  box-sizing: border-box;
+}
+
+.post {
+  width: 100%;
+}
+
+@media (min-width: 768px) {
+  .post {
+    width: 600px;
+    margin: auto;
+  }
+}
+
+.post-title {
+  margin: 0;
+}
+
+.post-details {
+  padding: 10px;
+  box-sizing: border-box;
+  border-bottom: 3px solid #ccc;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  flex-direction: column;
+}
+
+@media (min-width: 768px) {
+  .post-details {
+    flex-direction: row;
+  }
+}
+
+.post-detail {
+  color: rgb(88, 88, 88);
+  margin: 0 10px;
+}
+
+.post-feedback a {
+  color: red;
+  text-decoration: none;
+}
+
+.post-feedback a:hover,
+.post-feedback a:active {
+  color: salmon;
+}
+
+</style>
